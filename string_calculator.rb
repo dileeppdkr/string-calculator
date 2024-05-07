4474a5f2b640f71271832d0494c8ace404791e20
Dileep Kumar <dileep.pdkr@gmail.com>
2024-05-07 16:43:47 +0530

    multiple custom delimiters with length longer than one char

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,19 +3,15 @@ class StringCalculator
   def add(numbers_string)
     return  0 if numbers_string.empty?
     # get delimiter
-    delimiter = get_delimiter(numbers_string)
-    p "****delimiter***#{delimiter}"
-    # remove the delimiter prefix
-    numbers_without_delimiter = remove_delimiter_prefix(numbers_string)
-    p "****numbers_without_delimiter***#{numbers_without_delimiter}"
-    # Split the string using delimiter
-    numbers_array = numbers_without_delimiter.split(delimiter)
+    # check the delimiters and return the final array elements
+    numbers_array = multi_delimiter_check(numbers_string)
     # verify if any negative numbers
     negative_numbers = numbers_array.select { |num| num.to_i.negative? }
     # raise RuntimeError in case of negative numbers
     if negative_numbers.any?
       raise RuntimeError, "Negative numbers not allowed: #{negative_numbers.join(',')}"
     end
+    
     numbers = numbers_array.map(&:to_i)
     # ignores numbers bigger than 1000
     numbers = ignore_numbers_above_1000 numbers
@@ -24,27 +20,22 @@ class StringCalculator
 
 private
 
-  def get_delimiter numbers
+  def multi_delimiter_check numbers_string
     delimiter = /[\n,]/ # Default delimiter
-    if numbers.start_with?("//")
-      delimiter_match = numbers.match(/\[(.*?)\]/) #/\[([^\]]+)\]/
-      delimiter = Regexp.new(Regexp.escape(delimiter_match[1])) if delimiter_match
+    numbers = numbers_string.split(delimiter)
+    # check for any custom delimiter
+    if  numbers.first.include?('//')
+      # get the delimiter
+      delimiter = numbers.first.delete('//[]')
+      # delete the delimiters and craete number array
+      numbers = numbers.last.delete(delimiter).split('')
     end
-    delimiter
-  end
-
-  def remove_delimiter_prefix numbers
-    numbers.gsub!(/^\/\/.*?\n/, '')
+   
     numbers
   end
 
   def ignore_numbers_above_1000 numbers
-    numbers.collect! { |num| num >= 1000 ? num = 0 : num = num }
+    numbers.map! { |num| num >= 1000 ? 0 : num }
   end
 
 end
-
-# c = StringCalculator.new
-
-# p c.add('1\n2,3')
-# p c.add('//[;]\n1;2')
