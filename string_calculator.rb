682a51f511a897a8445b9adecad813ab50465d85
Dileep Kumar <dileep.pdkr@gmail.com>
2024-05-07 14:45:25 +0530

    Initial commit with readme, rspec, empty string returns 0

---
+++ string_calculator.rb
@@ -0,0 +1,8 @@
+class StringCalculator
+
+  def add(numbers_string)
+    return  0 if numbers_string.empty?
+    numbers = numbers_string.split(',').map(&:to_i)
+    return numbers.reduce(:+)
+  end
+end
